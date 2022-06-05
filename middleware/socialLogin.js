eab174f6ba0fd0d73c6e7e7a01280f8fd44ffc80
nalchiori <wodud00744@gmail.com>
2022-06-05 15:16:32 +0900

    revert to kakao login 7

--- middleware/socialLogin.js
+++ middleware/socialLogin.js
@@ -303,7 +303,7 @@ class KakaoLogin {
     this.socialKakaoClientSecret = socialKakaoClientSecret;
     this.socialKakaoRedirect = socialKakaoRedirect;
 
-    const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${this.socialKakaoClientId}&redirect_uri=${this.socialKakaoRedirect}&response_type=code&scope=account_email`;
+    const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${this.socialKakaoClientId}&redirect_uri=${this.socialKakaoRedirect}&response_type=code&scope=account_email, nickname`;
     return kakaoLoginUrl;
   }
   async auth (code) {
