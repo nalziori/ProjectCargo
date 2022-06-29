0871969bab4ee2778f598908be039030628dc5fd
nalchiori <wodud00744@gmail.com>
2022-06-29 15:50:54 +0900

    remove scope

--- middleware/socialLogin.js
+++ middleware/socialLogin.js
@@ -304,7 +304,7 @@ class KakaoLogin {
     this.socialKakaoClientSecret = socialKakaoClientSecret;
     this.socialKakaoRedirect = socialKakaoRedirect;
 
-    const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${this.socialKakaoClientId}&redirect_uri=${this.socialKakaoRedirect}&response_type=code&scope=account_email,name`;
+    const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${this.socialKakaoClientId}&redirect_uri=${this.socialKakaoRedirect}&response_type=codee`;
     return kakaoLoginUrl;
   }
   async auth (code) {
