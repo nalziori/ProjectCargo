0a7d6837c4a8a82bb2e2f3f56175961c45fb2cc6
nalchiori <wodud00744@gmail.com>
2022-06-30 13:44:51 +0900

    wrong word fixed

--- middleware/socialLogin.js
+++ middleware/socialLogin.js
@@ -304,7 +304,7 @@ class KakaoLogin {
     this.socialKakaoClientSecret = socialKakaoClientSecret;
     this.socialKakaoRedirect = socialKakaoRedirect;
 
-    const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${this.socialKakaoClientId}&redirect_uri=${this.socialKakaoRedirect}&response_type=codee`;
+    const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${this.socialKakaoClientId}&redirect_uri=${this.socialKakaoRedirect}&response_type=code`;
     return kakaoLoginUrl;
   }
   async auth (code) {
