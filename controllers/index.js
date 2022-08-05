f7364f264acb45d1ae5a1fa74669ff9f216e72c5
nalchiori <wodud00744@gmail.com>
2022-08-05 18:46:25 +0900

    u

--- controllers/index.js
+++ controllers/index.js
@@ -427,16 +427,7 @@ exports.index = doAsync(async (req, res, next) => {
         pageTitle: `${res.locals.setting.siteName}`,
         indexBoardGroups,
       });
-      var userId;
-      var user;
-      window.addEventListener("flutterInAppWebViewPlatformReady", function(event) {
-        window.flutter_inappwebview.callHandler('UserInfo', '*** Message From Server JS ***').then(function(result) {
-          //document.write(result.userId);
-          userId = result.userId;
-          user = req.session.user;
-        });
-      });
-      await conn.query("UPDATE user SET appToken=? WHERE uid=?", [userId, user.uid]);
+      
     } finally {
       conn.release();
     }
