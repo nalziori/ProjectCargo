ac4e97458cc303fc97f0aeee65383592ed699232
nalchiori <wodud00744@gmail.com>
2022-08-18 13:41:17 +0900

    a

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,10 @@
-importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
+if('serviceWorker' in navigator) {
+    window.addEventListener('load', ()=>{
+      navigator.serviceWorker.register('OneSignalSDKWorker.js')
+        .then((reg)=>{
+            importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
+            console.log('Service worker registered.', reg);
+        })
+        .catch(e => console.log(e));
+    })
+  }
