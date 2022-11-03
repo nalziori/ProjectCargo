b495e8ca30193480c604ad7661e468e437a45f96
nalchiori <wodud00744@gmail.com>
2022-11-03 19:13:40 +0900

    a

--- public/javascripts/read.js
+++ public/javascripts/read.js
@@ -494,7 +494,7 @@ const addEvent = async () => {
               var content = comment.querySelector('#etcText').value;
               const nickName = comment.querySelector('input[name="nickName"]')?.value;
               const password = comment.querySelector('input[name="password"]')?.value;
-              const doc = content;
+              var doc = content;
               var regURL = new RegExp("(http|https|ftp|telnet|news|irc)://([-/.a-zA-Z0-9_~#%$?&=:200-377()]+)","gi");
               content = doc.replace(regURL,"<a href='$1://$2' target='_blank' style='color: blue; text-decoration: underline; text-underline-position:under;'>$1://$2</a>");
               const data = {
